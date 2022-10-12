72cb0059ca5de66667f0837e0c6d44cdafdd836d
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-12 22:32:17 +0530

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuotesListComponent } from './quotes-list/quotes-list.component';
-
+const appRoutes:Routes=[
+  {
+    path:"",component:QuotesListComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +19,8 @@ import { QuotesListComponent } from './quotes-list/quotes-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
